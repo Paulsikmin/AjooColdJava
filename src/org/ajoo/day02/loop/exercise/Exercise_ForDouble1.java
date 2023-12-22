95ae80f2108f6c6823b75a8db8b06125425333c9
bongsik <minmediaservice@gmail.com>
2023-12-22 17:47:26 +0900

    [ADD] Java 2일차 - 제어문
    1. 조건문 if 예제코드
    2. 반복문 설명 및 예제코드
     2.1 for문
     2.2 while문
     2.3 do~while문
    3. 중첩 반복문 예제코드

---
+++ src/org/ajoo/day02/loop/exercise/Exercise_ForDouble1.java
@@ -0,0 +1,17 @@
+package org.ajoo.day02.loop.exercise;
+
+public class Exercise_ForDouble1 {
+	public static void main(String [] args) {
+		// 2단을 세로로 출력, 그 옆에 3단 세로로 출력,
+		// 그 옆에 4단 세로로 출력
+		for(int j = 1; j < 10; j++) {
+			for(int i = 2; i < 10; i++) {
+				System.out.print(i + " * "+j+" = "+i*j);
+				System.out.print("\t");
+			}
+			System.out.println();
+		}
+		
+		
+	}
+}
---
+++ src/org/ajoo/day02/loop/exercise/Exercise_ForDouble3.java
@@ -0,0 +1,25 @@
+package org.ajoo.day02.loop.exercise;
+
+public class Exercise_ForDouble3 {
+	public static void main(String [] args) {
+		// *
+		// **
+		// ***
+		// ****
+		// *****
+		// ******
+		int count = 1;
+		for(int i = 0; i < 6; i++) {
+			for(int j = 0; j < i+1; j++) {
+				System.out.print("*");
+			}
+			count++;
+			System.out.println();
+		}
+//		System.out.println("**");
+//		System.out.println("***");
+//		System.out.println("****");
+//		System.out.println("*****");
+//		System.out.println("******");
+	}
+}
